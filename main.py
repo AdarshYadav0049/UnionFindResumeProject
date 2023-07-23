5e42bda5b56ccf129af119769acbf1f34c881942
Adarshyadav0049 <adarshyadavyz2@gmail.com>
2023-07-23 16:59:01 +0530

    requirements

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ for edge in s:
     tup=(edge[0], edge[1])
     edges.append(tup)
 
-vid=MovingVertices(N=config_data['graph']['number_of_vertices'],EDGES=edges,LAY=config_data['graph']['layout'],GRAD= config_data['color']['gradient'], GS=config_data['color']['gradient_start'], GE =config_data['color']['gradient_end'])
+vid=MovingVertices(N=config_data['graph']['number_of_vertices'],EDGES=edges,LAY=config_data['graph']['layout'],
+                   GRAD= config_data['color']['gradient'],
+                   GS=config_data['color']['gradient_start'], GE =config_data['color']['gradient_end'])
 vid.render()
 play_video("media/videos/1080p60/MovingVertices.mp4")
